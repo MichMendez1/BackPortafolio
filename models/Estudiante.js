6b709edadb3cb033f2dbb7b16c82e6826986f7e3
ScarlethM <sc.martinezg@duocuc.cl>
2023-05-09 20:12:53 -0400

    Actualización

--- models/Estudiante.js
+++ models/Estudiante.js
@@ -1,4 +1,8 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose  from "mongoose";
+
+import bcrypt from 'bcrypt';
+import generarId from "../helpers/generarId.js";
+
 
 const estudianteSchema = mongoose.Schema({
     nombre: {
@@ -36,6 +40,7 @@ const estudianteSchema = mongoose.Schema({
     },
     token:{
         type: String,
+        default: generarId(),
 
     },
 
@@ -43,8 +48,23 @@ const estudianteSchema = mongoose.Schema({
         type: Boolean,
         default: false
     }
-
 });
+
+//Hashear clave
+estudianteSchema.pre('save', async function(next){
+
+    //Prevenir que lo vuelva a hashear
+    if (!this.isModified('password')){
+        next()
+    };
+   const salt = await bcrypt.genSalt(10);
+   this.password = await bcrypt.hash(this.password, salt);
+});
+
+//Confirmar si la clave coincide
+estudianteSchema.methods.comprobarPassword = async function (passwordFormulario){
+    return await bcrypt.compare(passwordFormulario, this.password)
+}
 
 const Estudiante = mongoose.model("Estudiante", estudianteSchema);
 export default Estudiante;
