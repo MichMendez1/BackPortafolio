37518dad5fd157118170e789973d7c94d347937c
Michael <mich.mendez@duocuc.cl>
2023-05-09 20:49:49 -0400

    Back de la scarleth

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+/*
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const cors = require('cors')
+*/
 
+import express from "express";
+import dotenv from 'dotenv';
+import conectarDB from './config/db.js';
+import estudianteRoutes from './routes/estudianteRoutes.js';
+import Estudiante from "./models/Estudiante.js";
+
+const app = express();
+app.use(express.json());
+
+//escanea y busca el archivo .env
+dotenv.config();
+conectarDB();
+
+app.use("/api/estudiantes", estudianteRoutes);
+
+const PORT = process.env.PORT ||4000
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor funcionando en el puerto ${PORT}`);
+});
+
+
+
+
+/*
 let users =[{
     Email:'Hola',
     Password:'Hola'
@@ -30,8 +57,7 @@ app.post('/', (req,res)=>{
 })
 
 
-
-
 app.listen(app.get('port'), ()=>{
     console.log(`Aplicacion funcionando en puerto ${app.get('port')}`);
 })
+*/
---
+++ models/Profesor.js
@@ -0,0 +1,71 @@
+import mongoose  from "mongoose";
+import bcrypt from 'bcrypt';
+import generarId from "../helpers/generarId.js";
+
+
+const profesorSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    rut: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique:true,
+        trim: true,
+    },
+
+    telfono: {
+        type: String,
+        default: null,
+        trim: true,
+    },
+
+    token:{
+        type: String,
+        default: generarId(),
+
+    },
+
+    confirmado:{
+        type: Boolean,
+        default: false
+    },
+
+    curso:{
+        type:String,
+
+    },
+});
+
+//Hashear clave
+profesorSchema.pre('save', async function(next){
+
+    //Prevenir que lo vuelva a hashear
+    if (!this.isModified('password')){
+        next()
+    };
+   const salt = await bcrypt.genSalt(10);
+   this.password = await bcrypt.hash(this.password, salt);
+});
+
+//Confirmar si la clave coincide
+profesorSchema.methods.comprobarPassword = async function (passwordFormulario){
+    return await bcrypt.compare(passwordFormulario, this.password)
+};
+
+const Profesor = mongoose.model("Profesor",profesorSchema);
+export default Profesor;
