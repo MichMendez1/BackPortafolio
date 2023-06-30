8d9e29a70f151961109e337c7f67bc385c5e22ff
alejandro <jano2.m3@gmail.com>
2023-06-30 15:32:33 -0400

    controller modificado

--- controllers/annoController.js
+++ controllers/annoController.js
@@ -1,22 +1,23 @@
-import Anno from "../models/Anno.js"
+import Dia from "../models/Dia.js";
+import generarJWT from "../helpers/generarJWT.js"
 
 const registrar =  async (req, res)=>{
 
 console.log(req.body);
-const {anno} = req.body;
+const {email} = req.body;
 
-//Prevenir años duplicados
-const existePeriodo = await  Anno.findOne({anno})
+//Prevenir usuarios duplicados
+const existeUsuario = await  Dia.findOne({email})
 
-if (existePeriodo){
-    const error = new Error('Año ya registrado');
-    return res.status(400).json({msg: error.message});
+if (existeUsuario){
+    const error = new Error('Usuario ya registrado');
+    return res.status(400).json({ errors: [{ msg: error.message }] });
 }
 try {
-    //Guardar nuevo Año
-    const anno = new Anno(req.body);
-    const annoGuardado = await anno.save(); 
-    res.json(annoGuardado);
+    //Guardar nuevo Dia
+    const dia = new Dia(req.body);
+    const diaGuardado = await dia.save(); 
+    res.json(diaGuardado);
 
 } catch (error) {
     console.log(error);    
@@ -25,35 +26,120 @@ try {
 };
 
 const perfil = (req, res)=>{
-    const { anno } = req;
-    res.json({ perfil : anno });
+    const { dia } = req;
+    res.json({ perfil : dia });
 };
 
-const confirmar = async (req,res)=>{
-    const { token } = req.params;
+// const confirmar = async (req, res) => {
+//     const { token } = req.params;
+  
+//     try {
+//       // Buscar usuario con ese token
+//       const usuarioConfirmar = await Dia.findOne({ token });
+  
+//       if (!usuarioConfirmar) {
+//         const error = new Error('Token no válido');
+//         return res.status(404).json({ msg: error.message });
+//       }
+  
+//       // Actualizar confirmado = true y guardar cambios
+//       usuarioConfirmar.confirmado = true;
+//       usuarioConfirmar.token = null;
+//       await usuarioConfirmar.save();
+  
+//       res.json({ msg: 'Usuario confirmado correctamente.' });
+//     } catch (error) {
+//       console.log(error);
+//       res.status(500).json({ msg: 'Error al confirmar el usuario.' });
+//     }
+//   };
+  
 
-    //Buscar año con ese token 
-    const periodoConfirmar = await Anno.findOne({token})
-    
-    if (!periodoConfirmar){
-        const error = new Error('Token no válido');
+const autenticar = async  (req, res)=>{
+    const{ email, password } = req.body
+
+    //Comprobar si el usuario existe 
+    const usuario = await Dia.findOne({email});
+    console.log(usuario);
+
+
+    if (!usuario){
+        const error = new Error('El Usuario no existe');
         return res.status(404).json({msg: error.message});
     }
 
-    try {
-        // Despues de buscar se modifica confirmado = true  y almacena 
-        periodoConfirmar.token = null;
-        periodoConfirmar.confirmado = true;
-        await periodoConfirmar.save()
-        res.json({msg: "Año confirmado."})
+    // //Comprobar si el usario esta confirmado 
+    // if (!usuario.confirmado){
+    //     const error = new Error('Tú cuenta no ha sido confirmada');
+    //     return res.status(403).json({msg: error.message});
+    // }
+
+    //Revisar el password 
+    if(await usuario.comprobarPassword(password)){
+        console.log('Contraseña correcta');
+        return res.status(200).json(usuario)
+    
+    //Autenticar
+        res.json({ token: generarJWT(usuario.id) });
+        const usuarioGuardado = {
+          _id: usuario.id,
+          nombres: usuario.nombres,
+          apellidoPaterno: usuario.apellidoPaterno,
+          apellidoMaterno: usuario.apellidoMaterno,
+          email: usuario.email,
+        };
         
-    } catch (error) {
-        console.log(error);
     }
+    else {
+        const error = new Error('Contraseña es incorrecta');
+        return res.status(403).json({msg: error.message});
+    }
+    
+   
 };
+
+
 
 const comprobarToken=(req, res)=>{
 
 } ;
 
-export {registrar, perfil, confirmar , comprobarToken};
+
+const obtenerUsuarios = async (req, res) => {
+    try {
+      const usuarios = await Dia.find(); // Obtiene todos los usuarios de la base de datos
+      res.json(usuarios); // Envía la lista de usuarios como respuesta
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Error al obtener los usuarios' });
+    }
+  };
+  const eliminarEstudiante = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const dia = await Dia.findByIdAndDelete(id);
+  
+      if (!dia) {
+        return res.status(404).json({ mensaje: 'Dia no encontrado' });
+      }
+  
+      res.json({ mensaje: 'Dia eliminado correctamente' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ mensaje: 'Error al eliminar el dia' });
+    }
+  };
+  const actualizarEstudiante = async (req, res) => {
+    const diaId = req.params.id;
+    try {
+      const dia = await Dia.findByIdAndUpdate(diaId, req.body, { new: true });
+      res.json(dia);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: 'Error al actualizar el dia' });
+    }
+  };
+  
+
+export {registrar, perfil, autenticar, comprobarToken, obtenerUsuarios,eliminarEstudiante, actualizarEstudiante};
